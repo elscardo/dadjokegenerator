2f75300a194ca8cc32eb5db0dd88ac92bc8d7e62
Michael Scardamaglia <mascardamaglia@gmail.com>
2021-04-07 21:18:34 -0400

    initial commit. Likely the only one.

---
+++ app.js
@@ -0,0 +1,20 @@
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLi = document.createElement('li');
+    newLi.append(jokeText);
+    jokes.append(newLi);
+}
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json', } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
+    } catch (e) {
+        return "No jokes are available!"
+    }
+}
+button.addEventListener('click', addNewJoke)
